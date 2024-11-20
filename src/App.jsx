c6c65de72938a890e83d7d2187d581d3a46ea6d6
WandyTheWand <joegfindlay@gmail.com>
2024-11-20 00:24:06 -0500

    Creacion del Componente "Productos"
    Se crea el Componente "Productos" y se le añaden Parte de los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Product } from './components/ProductCard'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+
+  const Products = [  {
+    id: 1,
+    name: "Sleek White & Orange Wireless Gaming Controller",
+    price: 69,
+    image: "https://i.imgur.com/ZANVnHE.jpeg",
+  },
+  {
+    id: 2,
+    name: "Sleek Wireless Headphone & Inked Earbud Set",
+    price: 44,
+    image: "https://i.imgur.com/yVeIeDa.jpeg",
+  },
+  {
+    id: 3,
+    name: "Sleek Comfort-Fit Over-Ear Headphones",
+    price: 28,
+    image: "https://i.imgur.com/SolkFEB.jpeg",
+  }]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className='products-section'>
+      {Products.map(productinfo => {
+            const {image, price, name} =  productinfo
+            return(
+              <Product
+                image={image}
+                price={price}
+                name={name} />
+            )
+          })
+          }
+      </section>
     </>
   )
 }
-
-export default App
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,21 @@
+export function Product({name, price, image}){
+  return(
+    <article className="product-card-body">
+      <header className="product-header">
+        <h1 className="product-name">{name}</h1>
+      </header>
+      <section className="product-image-border">
+        <img src={image} className="product-image" />
+      </section>
+      <section className="product-info">
+        <h2 className="product-price">{price}$</h2>
+        <h3 className="product-color">Colores:</h3>
+      </section>
+      <section className="product-unity">
+        <button className="minus-one-unity">-</button>
+        <span>1 Unidad(es)</span>
+        <button className="add-one-unity">+</button>
+      </section>
+    </article>
+  )
+}
