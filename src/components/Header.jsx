3623d5294f3207ce62f298f8249629e7af403e2e
WandyTheWand <joegfindlay@gmail.com>
2024-12-04 12:29:00 -0500

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export function Header(){
                 <h1 className="enterprise-logo">TecnoLife</h1>
             </div>
             <div className="search-product">
-                <input type="search" placeholder="Search for products" className="search-product-input"/>
+                <i className='bx bx-search'></i><input type="search" placeholder="Search for products" className="search-product-input"/>
             </div>
             <div className="page-links">
                 <ul className="page-links-list">
